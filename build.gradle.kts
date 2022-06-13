27849161d46ff6809079a969cb2190e5893e77b0
Vedran Pugar <vpugar@digital.ai>
2022-06-13 08:58:55 +0200

    Use versionToSync to synchronize nexus to dist

--- build.gradle.kts
+++ build.gradle.kts
@@ -133,7 +133,8 @@ tasks {
         dependsOn(named("buildOperators"), named("updateDocs"))
     }
 
-    register<Exec>("syncBlueprintsArchives") {
+
+    register<Exec>("copyBlueprintsArchives") {
         dependsOn("blueprintsArchives")
 
         if (project.hasProperty("versionToSync") && project.property("versionToSync") != "") {
@@ -144,12 +145,24 @@ tasks {
                         "cd /tmp/xl-op-blueprints/$versionToSync/;" +
                         "unzip -o /opt/sonatype-work/nexus/storage/digitalai-public/content/ai/digital/xlclient/blueprints/xl-op-blueprints/$versionToSync/xl-op-blueprints-$versionToSync.zip"
 
+            commandLine(commandUnzip.split(" "))
+        } else {
+            commandLine("echo",
+                "You have to specify which version you want to sync, ex. ./gradlew syncBlueprintsArchives -PversionToSync=22.0.0")
+        }
+    }
+
+    register<Exec>("syncBlueprintsArchives") {
+        dependsOn("blueprintsArchives", "copyBlueprintsArchives")
+
+        if (project.hasProperty("versionToSync") && project.property("versionToSync") != "") {
+            val versionToSync = project.property("versionToSync")
+
             val commandRsync =
                 "ssh xebialabs@nexus1.xebialabs.cyso.net rsync --update -raz -i --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r --include='*' " +
                         "/tmp/xl-op-blueprints/$versionToSync/ " +
                         "xldown@dist.xebialabs.com:/var/www/dist.xebialabs.com/public/xl-op-blueprints/$versionToSync"
 
-            commandLine(commandUnzip.split(" "))
             commandLine(commandRsync.split(" "))
         } else {
             commandLine("echo",
