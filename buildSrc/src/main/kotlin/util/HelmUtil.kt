4c3fec78b1348ecc57a199efa9ae767ee141edfe
Vedran Pugar <vpugar@digital.ai>
2023-03-31 08:56:16 +0200

    S-90924 fix support for paths with space

--- buildSrc/src/main/kotlin/util/HelmUtil.kt
+++ buildSrc/src/main/kotlin/util/HelmUtil.kt
@@ -8,19 +8,19 @@ class HelmUtil {
 
         fun helmRepo(project: Project) {
             ProcessUtil.executeCommand(project,
-                "helm repo add my-repo https://charts.bitnami.com/bitnami",
+                "helm repo add bitnami-repo https://charts.bitnami.com/bitnami",
                 logOutput = true, throwErrorOnFailure = true)
         }
 
         fun helmDeps(project: Project, chartDir: String) {
             ProcessUtil.executeCommand(project,
-                "helm dependency update $chartDir",
+                "helm dependency update \"$chartDir\"",
                 logOutput = true, throwErrorOnFailure = true)
         }
 
         fun helmPackage(project: Project, chartDir: String, targetDir: String): String {
             val result = ProcessUtil.executeCommand(project,
-                "helm package $chartDir --destination $targetDir",
+                "helm package $chartDir --destination \"$targetDir\"",
                 logOutput = true, throwErrorOnFailure = true)
             val version = result.substringAfterLast("$targetDir/remote-runner-", "")
                 .substringBefore(".tgz", "")
