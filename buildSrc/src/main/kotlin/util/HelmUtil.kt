19094575349021f634fa7f4f2435cad8ff196ea3
Vedran Pugar <72854396+vpugar-digital@users.noreply.github.com>
2024-03-12 15:25:05 +0100

    S-107468 preserve the naming of the remote-runner (#214)
    Co-authored-by: Vedran Pugar <vedran.pugar@digital.ai>

--- buildSrc/src/main/kotlin/util/HelmUtil.kt
+++ buildSrc/src/main/kotlin/util/HelmUtil.kt
@@ -2,6 +2,7 @@ package util
 
 import org.gradle.api.Project
 import java.lang.IllegalStateException
+import java.io.File
 
 class HelmUtil {
     companion object {
@@ -22,11 +23,14 @@ class HelmUtil {
             val result = ProcessUtil.executeCommand(project,
                 "helm package \"$chartDir\" --destination \"$targetDir\"",
                 logOutput = true, throwErrorOnFailure = true)
-            val version = result.substringAfterLast("$targetDir/remote-runner-", "")
+            val version = result.substringAfterLast("$targetDir/runner-", "")
                 .substringBefore(".tgz", "")
             if (version == "") {
                 throw IllegalStateException("Cannot get version from package output")
             }
+
+            // rename the RR to the legacy name
+            project.file("$targetDir/runner-$version.tgz").renameTo(File("$targetDir/remote-runner-$version.tgz"))
             return "remote-runner-$version.tgz"
         }
     }
